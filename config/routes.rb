010565240da39e4e78275dfa77fc3d0cc0041e16
Brenaoxline <brenomb07@hotmail.com>
2019-03-21 16:48:44 -0300

    V1.0

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :departamentos
+
+  get "produto/busca", to: "produtos#busca", as: :busca_produto
+  resources :produtos, only: [:new, :create, :destroy, :edit, :update]
+  root to: "produtos#index"
+end
